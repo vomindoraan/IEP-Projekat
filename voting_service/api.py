b13a2472695cbdfb4f7b980cd7d491ad9a6ebd9f
Konstantin Đorđević <vomindoraan@gmail.com>
2022-02-09 17:49:37 +0100

    Update voting service and daemon

--- voting_service/api.py
+++ voting_service/api.py
@@ -1,8 +1,8 @@
 import csv
 import io
+from uuid import UUID
 
 import redis
-from werkzeug.exceptions import ServiceUnavailable
 
 from common.api import *
 from . import config, schemas
@@ -19,17 +19,35 @@ def vote():
         f = request.files['file']
         stream = io.StringIO(f.stream.read().decode('utf-8'))
         csv_in = csv.reader(stream)
-    except (KeyError, UnicodeDecodeError) as e:
+    except KeyError as e:
         raise BadRequest("Field file missing.") from e
+    except UnicodeDecodeError as e:
+        raise BadRequest("Bad file encoding.") from e
 
+    # Check and gather ballot data
     content = []
     for i, values in enumerate(csv_in):
         if len(values) != 2:
-            return BadRequest(f"Incorrect number of values on line {i}.")
-        content.append(values)
+            raise BadRequest(f"Incorrect number of values on line {i}.")
 
+        try:
+            ballot_uuid = UUID(values[0])
+        except ValueError as e:
+            raise BadRequest(f"Incorrect ballot ID on line {i}.") from e
+
+        error = BadRequest(f"Incorrect poll number on line {i}.")
+        try:
+            poll_number = int(values[1])
+            if poll_number <= 0:
+                raise error
+        except ValueError as e:
+            raise error from e
+
+        content.append((ballot_uuid, poll_number))
+
+    # Publish ballot data to Redis
     r = redis.Redis(host=config.REDIS_HOST, port=config.REDIS_PORT)
     for values in content:
-        if r.publish(config.REDIS_VOTES_LIST, ' '.join(values)) == 0:
-            raise ServiceUnavailable("Voting service temporarily unavailable.",
-                                     retry_after=config.RETRY_DELAY)
+        r.publish(config.REDIS_VOTES_LIST, ' '.join(map(str, values)))
+        # raise ServiceUnavailable("Voting service temporarily unavailable.",
+        #                          retry_after=config.RETRY_DELAY)
--- voting_service/daemon.py
+++ voting_service/daemon.py
@@ -1,10 +1,11 @@
 if __name__ == '__main__':
     from datetime import datetime
+    from uuid import UUID
 
     import redis
 
     from . import config, create_app
-    from .models import DB, Election, Vote
+    from .models import DB, Election, Participant, Vote
 
 
     app = create_app()
@@ -15,21 +16,37 @@ if __name__ == '__main__':
     pubsub.subscribe(config.REDIS_VOTES_LIST)
 
     while True:
-        msg = pubsub.get_message()
+        msg = pubsub.get_message(ignore_subscribe_messages=True)
         try:
-            id, poll_number = msg.split()
-        except (AttributeError, ValueError):
+            values = msg['data'].decode('utf-8').split()
+            ballot_uuid = UUID(values[0])
+            poll_number = int(values[1])
+        except (KeyError, TypeError, AttributeError, UnicodeDecodeError):
             continue
 
-        now = datetime.now(config.TIMEZONE)
-        e = Election.query.filter(Election.start <= now <= Election.end).first()
-        if not e:
-            continue
+        with app.app_context():
+            now = datetime.now(config.TIMEZONE)
+            e = (
+                Election.query
+                .filter(Election.start <= now, now <= Election.end)
+                .first()  # TODO: What if there are multiple elections?
+            )
+            if not e:
+                continue
 
-        invalid = None
-        if Vote.query.get(id):
-            invalid = "Duplicate ballot."
+            invalid = None
+            if Vote.query.filter(Vote.ballot_uuid == ballot_uuid):
+                invalid = "Duplicate ballot."
 
-        vote = Vote(id, poll_number, invalid, election_id=e.id)
-        DB.session.add(vote)
-        DB.session.commit()
+            if not (
+                Participant.query
+                .filter(Participant.election_id == e.id,
+                        Participant.poll_number == poll_number)
+                .first()
+            ):
+                invalid = "Invalid poll number."
+
+            vote = Vote(ballot_uuid=ballot_uuid, poll_number=poll_number,
+                        invalid=invalid, election_id=e.id)
+            DB.session.add(vote)
+            DB.session.commit()
