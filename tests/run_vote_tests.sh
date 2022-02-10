eb9418b94dd98ee474535132d1561a586ddc12c7
Konstantin Djordjevic <dk140414@rti.lab>
2022-02-10 13:50:31 +0100

    0.0.0.0 -> localhost in tests

--- tests/run_vote_tests.sh
+++ tests/run_vote_tests.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python tests/main.py --authentication-address http://0.0.0.0:9000 --administrator-address http://0.0.0.0:9010 --station-address http://0.0.0.0:9020 --with-authentication --jwt-secret 13С113ИЕП --roles-field role --administrator-role admin --user-role user --type vote
+python tests/main.py --authentication-address http://localhost:9000 --administrator-address http://localhost:9010 --station-address http://localhost:9020 --with-authentication --jwt-secret 13С113ИЕП --roles-field role --administrator-role admin --user-role user --type vote
